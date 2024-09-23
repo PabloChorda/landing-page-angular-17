f89c5e88fe1edc7f327d4cd32213cfa62bb883b4
Pablo Chorda <pablochorda365@gmail.com>
2024-09-23 21:01:38 +0200

    Mira esta landing page en Angular 17

---
+++ src/app/pages/product-detail/product-detail.component.ts
@@ -0,0 +1,29 @@
+import { CommonModule } from '@angular/common';
+import { Component, OnInit, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ApiService } from '../../services/api.service';
+import { IProduct } from '../../models/product.model';
+
+@Component({
+  selector: 'app-product-detail',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './product-detail.component.html',
+  styleUrl: './product-detail.component.css',
+})
+export class ProductDetailComponent implements OnInit {
+  loading: boolean = true;
+  public product?: IProduct;
+
+  private _route = inject(ActivatedRoute);
+  private _apiService = inject(ApiService);
+
+  ngOnInit(): void {
+    this._route.params.subscribe((params) => {
+      this._apiService.getProduct(params['id']).subscribe((data: IProduct) => {
+        this.product = data;
+        this.loading = false;
+      });
+    });
+  }
+}
---
+++ src/app/pages/products/products.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit, inject } from '@angular/core';
+import { ApiService } from '../../services/api.service';
+import { CommonModule } from '@angular/common';
+import { IProduct } from '../../models/product.model';
+import { Router } from '@angular/router'
+
+@Component({
+  selector: 'app-products',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './products.component.html',
+  styleUrl: './products.component.css'
+})
+export class ProductsComponent implements OnInit {
+
+  productList: IProduct[] = []
+  private _apiService = inject(ApiService);
+  private _router = inject(Router);
+
+  ngOnInit(): void{
+    this._apiService.getProducts().subscribe((data: IProduct[]) => {
+      console.log(data)
+      this.productList = data;
+    }
+  );
+}
+navegate(id: number): void {
+  this._router.navigate(['/products', id]);
+}
+}
